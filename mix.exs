962933c61cd4ef3bcfb01b5294bc9f3b3f517698
Bryan Joseph <bryanjos@gmail.com>
2015-02-25 14:53:17 -0600

    Updated docs

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule ESTree.Mixfile do
 
   defp description do
     """
-    The SpiderMonkey AST Node definitions. Mostly following the ESTree Spec
+    The SpiderMonkey AST Node definitions. Mostly following the ESTree Spec. Targets ES6 definitions
     """
   end
 
